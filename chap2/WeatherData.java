a9ecf20133308cfd241708692158b793ea4305bb
Rohit Saxena <aerohit.saxena@gmail.com>
2011-06-10 17:08:15 +0530

    Removed some unused variables

--- chap2/WeatherData.java
+++ chap2/WeatherData.java
@@ -1,12 +1,9 @@
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Observable;
 
 public class WeatherData extends Observable {
 	private double temperature;
 	private double pressure;
 	private double humidity;
-	private List<Observer> observers;
 	
 	public double getTemperature() {
 		return temperature;
